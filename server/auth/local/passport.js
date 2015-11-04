286721a84a5b828bbb081e54705836648595aa15
gocoolkris <gokul.87@gmail.com>
2015-11-04 12:11:47 -0500

    authentication
    Conflicts:
    	server/config/passport/init.js
    	server/config/passport/login.js
    	server/config/passport/signup.js

---
+++ server/auth/local/passport.js
@@ -0,0 +1,37 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+exports.setup = function(User, config) {
+    passport.use(new LocalStrategy({
+            usernameField: 'username',
+            passwordField: 'password' // this is the virtual field on the model
+        },
+        function(username, password, done) {
+            User.findOne({
+                username: username.toLowerCase()
+            }, function(err, user) {
+                if (err) return done(err);
+
+                if (!user) {
+                    return done(null, false, {
+                        message: 'This username is not registered.'
+                    });
+                }
+                if (!user.authenticate(password)) {
+                    return done(null, false, {
+                        message: 'This password is not correct.'
+                    });
+                }
+
+                console.log('Checking if user is activated');
+                if (!user.activated) {
+                    return done(null, false, {
+                        message: 'Your account is not activated.  Check your email for an activation link.'
+                    });
+                }
+
+                return done(null, user);
+            });
+        }
+    ));
+};
--- server/config/express.js
+++ server/config/express.js
@@ -4,6 +4,7 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var cookieParser   = require('cookie-parser');
 var path           = require('path');
+var passport       = require('passport');
 
 module.exports = function(app) {
   app.use(bodyParser.urlencoded({
@@ -12,6 +13,7 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
+  app.use(passport.initialize());
 
   var assetsPath = path.normalize(__dirname + '/../../client/');
   app.use(express.static(assetsPath));;
